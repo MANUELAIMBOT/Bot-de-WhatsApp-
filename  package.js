a38ccc66e549bb51dcbbdd8386e6e07d65b448c3
MANUELAIMBOT <latinoc098@gmail.com>
2026-05-24 14:00:25 -0400

    Add files via upload

---
+++  package.js
@@ -0,0 +1,18 @@
+{
+  "name": "samuel-hack-store",
+  "version": "1.0.0",
+  "description": "Bot oficial de Samuel Hack Store",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "firebase": "^10.0.0",
+    "node-telegram-bot-api": "^0.65.0",
+    "@whiskeysockets/baileys": "^6.6.0",
+    "pino": "^8.19.0",
+    "axios": "^1.6.8",
+    "cheerio": "^1.0.0",
+    "ytdl-core": "^4.11.5"
+  }
+}
