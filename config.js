76b2d4621f132a415c67e2f53336aaf28bbf7f2a
MANUELAIMBOT <latinoc098@gmail.com>
2026-05-24 17:25:06 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js - Samuel Hack Store
+module.exports = {
+    telegramToken: "8751520013:AAG5OnvVbLSEx6JrgeTT2zVAz1bkLM7DjJc",
+    adminTelegramId: 8698439117,
+    adminWaNumbers: ["584166371131"],
+    firebaseConfig: {
+        apiKey: "AIzaSyB-J-8BUFhnDcZIEvYRNq6UAFaPcgxuAtE",
+        authDomain: "saku-store.firebaseapp.com",
+        databaseURL: "https://saku-store-default-rtdb.firebaseio.com",
+        projectId: "saku-store",
+        storageBucket: "saku-store.firebasestorage.app",
+        messagingSenderId: "815286002143",
+        appId: "1:815286002143:web:60e7c7770da491a6da646a",
+        measurementId: "G-W9EHSXZJVT"
+    }
+};
